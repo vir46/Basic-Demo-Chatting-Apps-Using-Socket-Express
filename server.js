55e1a8256c201ecb1cb26e786f1283fd94f6a3bc
Virtus Tan <50411627+virtustan@users.noreply.github.com>
2022-06-06 15:18:59 +0700

    Remove user online + Typing status function

--- server.js
+++ server.js
@@ -11,9 +11,10 @@ app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
 
+// initiated when a user connects to platform
 io.on('connection', function(socket){
 
-
+    // server validation for username
     socket.on('RegistName', function(name){
         if(alluser.indexOf(name) != -1){
             socket.emit('ServerRespond', 'This name is already used',false);
@@ -28,15 +29,29 @@ io.on('connection', function(socket){
 
     console.log('a user connected');
 
+    // initiate when a user disconnects
     socket.on('disconnect', function(){ 
         if(users[socket.id]){
             socket.broadcast.emit('userlog', users[socket.id] + ' has left the chat');
-            delete users[socket.id];
             var index = alluser.indexOf(users[socket.id]);
+            delete users[socket.id];
             alluser.splice(index,1);
+            io.emit('showOnline', alluser);
         }
         console.log('user disconnected');
     });
+
+    // broadcast to all client if someone typing
+    socket.on('typing', function(data){
+       socket.broadcast.emit('typing', data);
+    });
+    
+    // broadcast to all client if someone stop typing
+    socket.on('stoptyping', function(data){
+        socket.broadcast.emit('stopTyping', data);
+    });
+
+    // broadcast to all client if someone sending message
     socket.on('Pesanmasuk', function(user,msg){
         io.emit('pesanbaru', user,msg);
         console.log(user + ' : ' + msg);
